0aead46007070e64f19a76676b1c4f7c5b7ccbc0
Benjamín R. Moreno Ortiz <benjamin_moreno_ortiz@alumni.brown.edu>
2023-01-27 11:05:16 -0600

    Create PENI-Ene-27.cpp

---
+++ PENI-Ene-27.cpp
@@ -0,0 +1,14 @@
+// Este es mi primer programa.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// tipoDeRetorno indentificador () 
+int main () {
+    string nombre;   
+    cout << "¿Cual es tu nombre?"<<endl;
+    cin >> nombre;
+    cout << "Hola " <<nombre << "!";
+    return 0;
+}
